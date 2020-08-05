28b91938d324df3b5d56f39e40d5ad5d599ac929
Lingani Tshuma <tshumal@gmail.com>
2020-08-05 14:26:03 +0200

    constructor observable with next, error and complete methods

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-alert('Hello RXJS');
+import {Observable} from "rxjs";
+import {allBooks} from "./data";
+
+let allBooksObservable$ = new Observable(subscriber => {
+    if(document.title !== 'RxBookTracker'){
+        subscriber.error('Incorrect Page Title');
+    }
+
+    for(let book of allBooks){
+        subscriber.next(book)
+    }
+
+    setTimeout(() => {
+        subscriber.complete();
+    }, 2000);
+
+    return () => console.log('Executing teardown code.')
+
+});
+
+// @ts-ignore
+allBooksObservable$.subscribe(book => console.log(book.title));
